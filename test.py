e6ae91b58ed88600e60c4cb1a4f145b91cee5fd4
XueFengZeng <xuefeng.zeng@hypereal.com>
2017-03-13 17:24:30 +0800

    Init repo.

---
+++ test.py
@@ -0,0 +1,40 @@
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("MNIST_data/", reshape=False)
+X_train, y_train           = mnist.train.images, mnist.train.labels
+X_validation, y_validation = mnist.validation.images, mnist.validation.labels
+X_test, y_test             = mnist.test.images, mnist.test.labels
+
+assert(len(X_train) == len(y_train))
+assert(len(X_validation) == len(y_validation))
+assert(len(X_test) == len(y_test))
+
+print()
+print("Image Shape: {}".format(X_train[0].shape))
+print()
+print("Training Set:   {} samples".format(len(X_train)))
+print("Validation Set: {} samples".format(len(X_validation)))
+print("Test Set:       {} samples".format(len(X_test)))
+
+import numpy as np
+
+# Pad images with 0s
+print("Origin Image Shape: {}".format(X_train.shape))
+X_train = np.pad(X_train, ((0, 0), (2, 2), (2, 2), (0, 0)), 'constant')
+X_validation = np.pad(X_validation, ((0, 0), (2, 2), (2, 2), (0, 0)), 'constant')
+X_test = np.pad(X_test, ((0, 0), (2, 2), (2, 2), (0, 0)), 'constant')
+
+print("Updated Image Shape: {}".format(X_train.shape))
+
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+# np.set_printoptions(threshold=np.nan)
+
+index = random.randint(0, len(X_train))
+image = X_train[index].squeeze()
+print(image)
+
+plt.figure(figsize=(1,1))
+plt.imshow(image, cmap="gray")
+print(y_train[index])
